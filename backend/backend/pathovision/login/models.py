8fbd95b620ca87d55a29dfbb67acf7c840222243
Palaniswamy <ArjunPalaniswamy@my.unt.edu>
2024-04-30 22:05:30 -0500

    updated

--- backend/backend/pathovision/login/models.py
+++ backend/backend/pathovision/login/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 class UserCredentials(models.Model):
-    user_id = models.AutoField(primary_key=True)
+    # user_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=200, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=200)
